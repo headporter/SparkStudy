b7855c78fc61b1cc37cf52e2a82005803c04f867
1002516 <1002516@SKP1002516MN001.local>
2017-11-20 13:29:17 +0900

    basic df,ds

--- src/study/lsw/sql/basic.scala
+++ src/study/lsw/sql/basic.scala
@@ -6,16 +6,19 @@ object basic {
   
   def main(args : Array[String]) {
     
-    val saprk = SparkSession
+    val spark = SparkSession
       .builder()
       .appName("basic")
       .master("local[*]")
+      .config("spark.driver.host", "127.0.0.1")
       .getOrCreate()
       
     val data = "./data"
-    val df = saprk.read.text(data)
+    val df = spark.read.text(data)
     
-    runUntypedDFTransformation(df)
+    //runUntypedDFTransformation(df)
+    runTypedDSTransformation(spark,df)
+  }
     
     // df.show()
     def runUntypedDFTransformation(df:DataFrame) : Unit = {
@@ -27,6 +30,15 @@ object basic {
       result.printSchema()
     }
     
-  }
+    def runTypedDSTransformation(spark: SparkSession,df:DataFrame) : Unit = {
+      import spark.implicits._
+      
+      val ds = df.as[(String)]
+      val result = ds.flatMap(_.split(" "))
+                      .groupByKey(v => v)
+                      .count()
+       result.show()               
+    }
   
 }
+  
