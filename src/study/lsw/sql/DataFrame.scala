469f202d97cefad6fda945267e327adaee624e43
1002516 <1002516@SKP1002516MN001.local>
2017-11-20 18:55:33 +0900

    upgrade df

--- src/study/lsw/sql/DataFrame.scala
+++ src/study/lsw/sql/DataFrame.scala
@@ -2,6 +2,7 @@ package study.lsw.sql
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types._
 
 object DataFrame {
   def main (args:Array[String]) = {
@@ -23,6 +24,14 @@ object DataFrame {
     val df = spark.read.csv(sparkHomeDir+"/person.csv")
     
     df.show()
+    
+    val sf1 = StructField("name", StringType, nullable = true)
+    val sf2 = StructField("age",  IntegerType, nullable = true)
+    val sf3 = StructField("job",  StringType, nullable = false)
+    val schema = StructType(List(sf1, sf2, sf3))
+    
+    val df1 = spark.read.schema(schema).csv(sparkHomeDir+"/person.csv")
+    df1.show()
   }
   
 }
